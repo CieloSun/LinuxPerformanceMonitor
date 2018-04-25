0f5a03e1feb351116cb88cdeb743765ddaa82b71
lyf94 <liyongfa1994@sina.com>
2018-04-25 17:32:56 +0800

    add web

---
+++ Web/doTest.php
@@ -0,0 +1,16 @@
+<?php
+$json = '[
+  {
+    "time": 1524560053410,
+    "value": 0.06370002
+  }
+  
+]';
+	
+	// $type = $_POST['types'];
+  // var_dump($type);
+	//首先将json字符串转换成数组
+	// $arr = json_decode($json, true); 
+	// 定义一个二维数组用于保存符合条件的点
+	echo $json;
+?>
